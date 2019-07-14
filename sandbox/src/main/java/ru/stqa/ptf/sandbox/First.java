8fb29b9cf9d9b73f7551ab8bbba88b833935e8dd
Pavel Kabanov <pkabanov.d@gmail.com>
2019-07-14 15:23:36 +0300

    Added some constructors

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/First.java
@@ -3,26 +3,25 @@ package ru.stqa.ptf.sandbox;
 public class First {
     public static void main(String args[]){
         String name = "Dima";
-        hello(name + 3);
+        hello(name);
 
-        double LEN = 5;
-        System.out.println("Площадь квадрата со стороной " + LEN + " = " + area(LEN));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
     }
 
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(double oh){
-        return oh *oh;
+    public static double area(Square s){
+        return s.l * s.l;
     }
 
-    public static  double area(double a, double b) {
-        return a * b;
+    public static  double area(Rectangle r) {
+        return r.a * r.b;
     }
 
 }
