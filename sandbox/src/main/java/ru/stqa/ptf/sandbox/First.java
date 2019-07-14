448c05cb2c07a9cb9bc8f9d0f2a3fd91f6909e25
Pavel Kabanov <pkabanov.d@gmail.com>
2019-07-14 15:00:34 +0300

    Add some functions

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/First.java
@@ -2,6 +2,27 @@ package ru.stqa.ptf.sandbox;
 
 public class First {
     public static void main(String args[]){
-        System.out.println("Hi!");
+        String name = "Dima";
+        hello(name + 3);
+
+        double LEN = 5;
+        System.out.println("Площадь квадрата со стороной " + LEN + " = " + area(LEN));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
     }
+
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double oh){
+        return oh *oh;
+    }
+
+    public static  double area(double a, double b) {
+        return a * b;
+    }
+
 }
